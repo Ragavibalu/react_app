c7245fa884a5761731f6dc6547d0021f9573b480
Ragavibalu <ragavibalu66@gmail.com>
2024-01-25 18:38:24 +0530

    reactrouters

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import PropsComponent from './components/functionalcomponents/PropsComponents';
-import StateComponent from './components/classcomponents/StateComponent';
+//import PropsComponent from './components/functionalcomponents/PropsComponents';
+//import StateComponent from './components/classcomponents/StateComponent';
+import NavBar from './components/functionalcomponents/NavBar';
+import { BrowserRouter ,Routes, Route } from 'react-router-dom';
+import About from './components/functionalcomponents/About';
+import Experience from './components/functionalcomponents/Experience';
+import Home from './components/functionalcomponents/Home';
+import Login from './components/functionalcomponents/Login';
+
 function App() {
   return (
     <div className="App">
-     <PropsComponent name="Rags" course="MERN course"></PropsComponent>
+      <BrowserRouter>
+      <NavBar/>
+      <Routes>
+      <Route path="/experience" element={<Experience />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/Home" element={<Home />}></Route>
+          <Route path="/Login" element={<Login />}></Route>
+      </Routes>
+      </BrowserRouter>
+      {/*<PropsComponent name="Hello everyone" course="ECE"></PropsComponent>*/}
       <header className="App-header">
-        <StateComponent/>
+       {/*<Statecomponent/>*/}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -21,6 +37,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer>
+      <p>&copy; 2024 All Rights Reserved By <span>authorized</span></p>
+      </footer>
     </div>
   );
 }
--- src/components/functionalcomponents/PropsComponents.jsx
+++ src/components/functionalcomponents/PropsComponents.jsx
@@ -1,6 +1,12 @@
+import "../css/PropsComponent.css"
 function PropsComponent(props){
+    const StyleAttrs={
+        backgroundColor:"#282c34",
+        color:"white"
+
+    }
     return(
-        <div>
+        <div style={StyleAttrs}>
             <h1>Hello,{props.name} <br></br>This is again {props.course} class</h1> 
         </div>
     )
